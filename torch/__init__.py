e8c4f1db9fc36f0ea3e79587187bf7edb1ba2423
Edward Z. Yang <ezyang@fb.com>
2022-11-10 10:29:56 -0500

    Symbolic shape floor and sym_sqrt
    ghstack-source-id: 30d58f4f9785fbb201d9927fd0ecab6f797cfba7
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/88760

--- torch/__init__.py
+++ torch/__init__.py
@@ -29,6 +29,8 @@ else:
 
 from ._six import string_classes as _string_classes
 
+from torch.types import _bool
+from torch._prims_common import NumberType
 from typing import Set, Type, TYPE_CHECKING, Union, Callable, Any
 import builtins
 
@@ -218,8 +220,23 @@ class SymInt:
 
     # Magic methods installed by torch.fx.experimental.symbolic_shapes
 
+    def __eq__(self, other: object) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __lt__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __gt__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __le__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __ge__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
+
     def __sym_float__(self):
-        ...
+        raise AssertionError("type stub not overridden")
 
     def __repr__(self):
         return self.node.str()
@@ -247,8 +264,20 @@ class SymFloat:
 
     # Magic methods installed by torch.fx.experimental.symbolic_shapes
 
-    def __sym_int__(self):
-        ...
+    def __eq__(self, other: object) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __lt__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __gt__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __le__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
+
+    def __ge__(self, other: NumberType) -> _bool:
+        raise AssertionError("type stub not overridden")
 
     def __repr__(self):
         return self.node.str()
